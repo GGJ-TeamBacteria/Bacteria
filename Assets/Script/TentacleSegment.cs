c33038f3ed29387c9f598c9c63546cdee87ddfaf
ErinDuXiao <suikatei@gmail.com>
2018-01-27 14:30:38 -0800

    hand extending movement

---
+++ Assets/Script/TentacleSegment.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TentacleSegment : MonoBehaviour {
+
+    internal float distanceFromPlayer;
+
+}
+
+
+//// exetend the arm
+//TentacleSegment currentSegment = Instantiate(armPrefab, nextSpawnPoint.position + (direction * distanceOfTentacles), nextSpawnPoint.rotation);
+//armParts.Add(currentSegment);
+//            currentSegment.distanceFromPlayer = (currentSegment.transform.position - gameObject.transform.position).magnitude;
+
+
+//if (Input.GetKey("2"))
+//{
+//    if (armParts.Count == 0)
+//    {
+//        return;
+//    }
+
+//    armParts.RemoveAt(armParts.Count-1);
+//}
