0d34b80439b8b261bef3a9d9d19d31e4f8cbe33c
Dee Gelbart <dee.gelbart@gmail.com>
2018-01-27 16:56:59 -0800

    torso locator

---
+++ Assets/Scripts/GestureTrigger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class GestureTrigger : MonoBehaviour
+{
+    public AudioClip[] stretchAttackSounds;
+    AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Trigger events are only sent if one of the Colliders also has a Rigidbody attached. Kinematic is OK
+        audioSource = GetComponent<AudioSource>();
+        AudioClip attackSound = stretchAttackSounds[Random.Range(0, stretchAttackSounds.Length)];
+        audioSource.PlayOneShot(attackSound);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+
+    }
+
+    //GestureTriggerPrefab is a gameobject a prefab
+    //GestureTrigger.cs
+    //GestureTriggerColliderExample
+
+
+
+
+}
---
+++ Assets/Scripts/LocatePlayerBody.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocatePlayerBody : MonoBehaviour {
+
+    public GameObject cameraHead;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Torso is below head
+        transform.position = cameraHead.transform.position;
+        transform.Translate(Vector3.down * 1, Space.World);
+    }
+}
