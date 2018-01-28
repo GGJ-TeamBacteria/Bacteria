7e99b243526f45f04268b46595570dc4438ebcda
zealyim <Zeal@HWP.local>
2018-01-27 16:56:14 -0800

    uploading asset

---
+++ Assets/Zeal/Scripts/BacteriaMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BacteriaMovement : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().velocity = transform.forward * Random.Range(-.5f, .5f) * speed;
+		GetComponent<Rigidbody>().velocity = transform.right * Random.Range(-.5f, .5f) * speed;
+		GetComponent<Rigidbody>().velocity = transform.up * Random.Range(-.5f, .5f) * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void fixedUpdate() {
+		
+	}
+}
