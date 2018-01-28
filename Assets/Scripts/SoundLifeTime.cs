248475f395cece225469a2248bc5fd05b19d3684
ErinDuXiao <suikatei@gmail.com>
2018-01-28 15:49:18 -0800

    Adding bacteria sound

---
+++ Assets/Scripts/SoundLifeTime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundLifeTime : MonoBehaviour {
+
+    public float maxLifeTime;
+    private float lifeTime;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lifeTime += Time.deltaTime;
+        if (lifeTime >= maxLifeTime)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
