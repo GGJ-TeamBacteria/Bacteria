c50c7285cefe3a31e2865424fbc8627e494d4cb4
ErinDuXiao <suikatei@gmail.com>
2018-03-18 18:42:13 -0800

    Adding tentacle animation for absorbing

--- Assets/Scripts/TentacleSegment.cs
+++ Assets/Scripts/TentacleSegment.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TentacleSegment : MonoBehaviour {
 
     internal Tentacle rootTentacle;
+    private bool isAnimating;
 
     void OnTriggerEnter(Collider other)
     {
@@ -32,21 +33,41 @@ public class TentacleSegment : MonoBehaviour {
             //Destroy(other.gameObject);
         }
     }
+
+    IEnumerator AbsorbingAnimation()
+    {
+        isAnimating = true;
+
+        gameObject.transform.localScale *= 1.2f;
+        yield return new WaitForSeconds(0.05f);
+        gameObject.transform.localScale *= 1.25f;
+        yield return new WaitForSeconds(0.05f);
+        gameObject.transform.localScale *= 1.3f;
+        yield return new WaitForSeconds(0.05f);
+        gameObject.transform.localScale *= 1.35f;
+        yield return new WaitForSeconds(0.05f);
+
+        gameObject.transform.localScale /= 1.35f;
+        yield return new WaitForSeconds(0.08f);
+        gameObject.transform.localScale /= 1.3f;
+        yield return new WaitForSeconds(0.08f);
+        gameObject.transform.localScale /= 1.25f;
+        yield return new WaitForSeconds(0.08f);
+        gameObject.transform.localScale /= 1.2f;
+
+        isAnimating = false;
+    }
+
+    IEnumerator AttackedAnimation()
+    {
+        isAnimating = true;
+
+        gameObject.transform.localScale /= 2.0f;
+        yield return new WaitForSeconds(0.3f);
+
+        gameObject.transform.localScale *= 2.0f;
+
+        isAnimating = false;
+    }
 }
 
-
-//// exetend the arm
-//TentacleSegment currentSegment = Instantiate(armPrefab, nextSpawnPoint.position + (direction * distanceOfTentacles), nextSpawnPoint.rotation);
-//armParts.Add(currentSegment);
-//            currentSegment.distanceFromPlayer = (currentSegment.transform.position - gameObject.transform.position).magnitude;
-
-
-//if (Input.GetKey("2"))
-//{
-//    if (armParts.Count == 0)
-//    {
-//        return;
-//    }
-
-//    armParts.RemoveAt(armParts.Count-1);
-//}
