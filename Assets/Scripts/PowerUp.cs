e21e7d8ffcabb6f5fcca433d92fbf0560a0ae774
SevernayaZemlya <jay.aguera@gmail.com>
2018-03-30 14:35:55 -0700

    powerup template script

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour {
+
+    public float m_Duration;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public float GetDuration()
+    {
+        return m_Duration;
+    }
+}
