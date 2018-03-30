1004116863abe69d22e826faf702751c68f9c0f1
SevernayaZemlya <jay.aguera@gmail.com>
2018-03-30 14:52:19 -0700

    powerup script update

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PowerUp : MonoBehaviour {
 
+    public enum PowerUpType { Extend, Super, Health};
+
     public float m_Duration;
+    public PowerUpType m_type;
 
 	// Use this for initialization
 	void Start () {
@@ -19,5 +22,15 @@ public class PowerUp : MonoBehaviour {
     public float GetDuration()
     {
         return m_Duration;
+    }
+
+    public PowerUpType GetPowerUpType()
+    {
+        return m_type;
+    }
+
+    public void SetPowerUpType(PowerUpType type)
+    {
+        m_type = type;
     }
 }
