a40b2dfcd7dcd72512bff499b05cdaad39626ab0
SevernayaZemlya <jay.aguera@gmail.com>
2018-03-30 15:27:12 -0700

    powerup interface, baby prefabs

---
+++ Assets/Scripts/PowerUps/PowerUp.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface PowerUp
+{
+
+    void AffectPlayer(Tentacle tentacle);
+
+   
+}
---
+++ Assets/Scripts/PowerUps/PowerUpExtend.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpExtend : MonoBehaviour, PowerUp {
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AffectPlayer(Tentacle tentacle)
+    {
+        tentacle.ExtendMaxLength(1);
+    }
+}
