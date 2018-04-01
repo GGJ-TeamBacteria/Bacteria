c2dad0968c8026d1dd5efdfcf3075a432f07880d
zealyim <swdreamwa@hotmail.com>
2018-04-01 12:04:13 -0700

    shooter rotates

--- Assets/Scripts/Bacteria/ShoBatMovement.cs
+++ Assets/Scripts/Bacteria/ShoBatMovement.cs
@@ -10,11 +10,11 @@ public class ShoBatMovement : MonoBehaviour {
 
     
     public Transform center;
-    public Vector3 axis = Vector3.right;
+    public Vector3 axis = Vector3.up;
     public Vector3 desiredPosition;
     public float radius = 2.0f;
     public float radiusSpeed = 0.5f;
-    public float rotationSpeed = 80.0f;
+    public float rotationSpeed = 20.0f;
 
     // Use this for initialization
     void Start () {
@@ -23,8 +23,8 @@ public class ShoBatMovement : MonoBehaviour {
 
         
         
-        transform.position = (transform.position - center.position).normalized * radius + center.position;
-        radius = 2.0f;
+        //transform.position = (transform.position - center.position).normalized * radius + center.position;
+
     }
 	
 	// Update is called once per frame
@@ -49,7 +49,7 @@ public class ShoBatMovement : MonoBehaviour {
         //}
 
         transform.RotateAround(center.position, axis, rotationSpeed * Time.deltaTime);
-        desiredPosition = (transform.position - center.position).normalized * radius + center.position;
-        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
+        //desiredPosition = (transform.position - center.position).normalized * radius + center.position;
+        //transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
     }
 }
